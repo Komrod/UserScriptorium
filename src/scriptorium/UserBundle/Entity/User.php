6b53947cf107c117df1f0651b6c4e9864a3d9a80
Thierry de Lapeyre <tdelapeyre.ext@orange.com>
2014-10-08 17:36:59 +0200

    Profile page start

--- src/scriptorium/UserBundle/Entity/User.php
+++ src/scriptorium/UserBundle/Entity/User.php
@@ -6,6 +6,8 @@ use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Security\Core\Util\SecureRandom;
 
 /**
  * @ORM\Entity
@@ -25,14 +27,38 @@ class User extends BaseUser
      */
     protected $registrationDate;
     
+    /**
+     * @ORM\Column(name="edition_date", type="datetime")
+     */
+    protected $editionDate;
+    
+    /**
+     * @Assert\File(maxSize="2048k")
+     * @Assert\Image(mimeTypesMessage="Please upload a valid image.")
+     */
+    protected $profilePictureFile;
+    
+    // for temporary storage
+    private $tempProfilePicturePath;
+    
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $profilePicturePath;
+    
+        
     public function __construct()
     {
         parent::__construct();
         
         // default registration date
         $this->registrationDate = new \DateTime();
+        
+        // default edition date
+        $this->editionDate = new \DateTime();
     }
-
+    
+    
     /**
      * Get id
      * @return integer
@@ -42,14 +68,9 @@ class User extends BaseUser
         return $this->id;
     }
     
-    public function hasName()
-    {
-        return false;
-    }
-
     /**
-     * Get registrationDate
-     * @return string
+     * Get registration date
+     * @return DateTime
      */
     public function getRegistrationDate()
     {
@@ -57,7 +78,7 @@ class User extends BaseUser
     }
     
     /**
-     * Set registrationDate
+     * Set registration date
      * @param $date DateTime The registration date
      */
     public function setRegistrationDate($date)
@@ -65,4 +86,210 @@ class User extends BaseUser
         $this->registrationDate = $date;
     }
     
+    /**
+     * Get edition date
+     * @return DateTime
+     */
+    public function getEditionDate()
+    {
+        return $this->editionDate;
+    }
+    
+    /**
+     * Set edition date
+     * @param $date DateTime The edition date
+     */
+    public function setEditionDate($date)
+    {
+        $this->registrationDate = $date;
+    }
+    
+    /**
+     * Set registrationDate
+     * @param $date DateTime The registration date
+     */
+    public function setEditionDateNow()
+    {
+        $this->setEditionDate(new \DateTime());
+    }
+    
+    
+/**
+     * Sets the file used for profile picture uploads
+     * 
+     * @param UploadedFile $file
+     * @return object
+     */
+    public function setProfilePictureFile(UploadedFile $file = null) 
+    {
+        // set the value of the holder
+        $this->profilePictureFile = $file;
+        
+         // check if we have an old image path
+        if (isset($this->profilePicturePath)) 
+        {
+            // store the old name to delete after the update
+            $this->tempProfilePicturePath = $this->profilePicturePath;
+            $this->profilePicturePath = null;
+        } else 
+        {
+            $this->profilePicturePath = 'initial';
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the file used for profile picture uploads
+     * 
+     * @return UploadedFile
+     */
+    public function getProfilePictureFile() 
+    {
+        return $this->profilePictureFile;
+    }
+
+    /**
+     * Set profilePicturePath
+     *
+     * @param string $profilePicturePath
+     * @return User
+     */
+    public function setProfilePicturePath($profilePicturePath)
+    {
+        $this->profilePicturePath = $profilePicturePath;
+
+        return $this;
+    }
+
+    /**
+     * Get profilePicturePath
+     *
+     * @return string 
+     */
+    public function getProfilePicturePath()
+    {
+        return $this->profilePicturePath;
+    }
+
+    
+    /**
+     * Get the absolute path of the profilePicturePath
+     */
+    public function getProfilePictureAbsolutePath() 
+    {
+        return null === $this->profilePicturePath
+            ? null
+            : $this->getUploadRootDir().'/'.$this->profilePicturePath;
+    }
+
+    /**
+     * Get root directory for file uploads
+     * 
+     * @return string
+     */
+    protected function getUploadRootDir($type='profilePicture')
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir($type);
+    }
+
+    /**
+     * Specifies where in the /web directory profile pic uploads are stored
+     * 
+     * @return string
+     */
+    protected function getUploadDir($type='profilePicture') 
+    {
+        // the type param is to change these methods at a later date for more file uploads
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/user/profilepics';
+    }
+
+    /**
+     * Get the web path for the user
+     * 
+     * @return string
+     */
+    public function getWebProfilePicturePath() 
+    {
+
+        return '/'.$this->getUploadDir().'/'.$this->getProfilePicturePath(); 
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUploadProfilePicture() 
+    {
+        if (null !== $this->getProfilePictureFile()) 
+        {
+            // a file was uploaded
+            // generate a unique filename
+            $filename = $this->generateRandomProfilePictureFilename();
+            $this->setProfilePicturePath($filename.'.'.$this->getProfilePictureFile()->guessExtension());
+        }
+    }
+
+    /**
+     * Generates a 32 char long random filename
+     * 
+     * @return string
+     */
+    public function generateRandomProfilePictureFilename() 
+    {
+        $count = 0;
+        do 
+        {
+            $generator = new SecureRandom();
+            $random = $generator->nextBytes(16);
+            $randomString = bin2hex($random);
+            $count++;
+        }
+        while (file_exists($this->getUploadRootDir().'/'.$randomString.'.'.$this->getProfilePictureFile()->guessExtension()) && $count < 50);
+
+        return $randomString;
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     * 
+     * Upload the profile picture
+     * 
+     * @return mixed
+     */
+    public function uploadProfilePicture() {
+        // check there is a profile pic to upload
+        if ($this->getProfilePictureFile() === null) {
+            return;
+        }
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getProfilePictureFile()->move($this->getUploadRootDir(), $this->getProfilePicturePath());
+
+        // check if we have an old image
+        if (isset($this->tempProfilePicturePath) && file_exists($this->getUploadRootDir().'/'.$this->tempProfilePicturePath)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->tempProfilePicturePath);
+            // clear the temp image path
+            $this->tempProfilePicturePath = null;
+        }
+        $this->profilePictureFile = null;
+    }
+
+     /**
+     * @ORM\PostRemove()
+     */
+    public function removeProfilePictureFile()
+    {
+        if ($file = $this->getProfilePictureAbsolutePath() && file_exists($this->getProfilePictureAbsolutePath())) {
+            unlink($file);
+        }
+    }
+
 }
